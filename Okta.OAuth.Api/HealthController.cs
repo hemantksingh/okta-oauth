4f6746fd0c62af11552a909769076d9749c4edd9
hemantksingh <hemantksingh.hk@gmail.com>
2017-08-15 12:37:42 +0100

    Adds health endpoint.

---
+++ Okta.OAuth.Api/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using System.Web.Http;
+
+namespace Okta.OAuth.Api
+{
+	[Route("health")]
+	public class HealthController : ApiController
+	{
+		public IHttpActionResult Get()
+		{
+			Version assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
+			
+			return Json( new
+				{
+					status = HttpStatusCode.OK,
+					version = assemblyVersion.ToString()
+				});
+		}
+	}
+}
---
+++ Okta.OAuth.CodeFlow/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Okta.OAuth.CodeFlow.Controllers
+{
+	public class HealthController : Controller
+    {
+        public ActionResult Index()
+        {
+			Version assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+			return Json(
+				new {
+					status = HttpStatusCode.OK,
+					version = assemblyVersion.ToString()
+				}, 
+				JsonRequestBehavior.AllowGet);
+		}
+    }
+}
