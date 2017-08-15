8ce3074421ebce962d75b36a47fbd6962505218e
hemantksingh <hemantksingh.hk@gmail.com>
2017-08-15 13:17:57 +0100

    Updates app version in build.

---
+++ update-version.ps1
@@ -0,0 +1,27 @@
+param (
+  [Parameter(mandatory=$true)]
+  [string]$appName,
+  [Parameter(mandatory=$true)]
+  [string]$buildNumber
+)
+$path = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$assemblyFile = (Join-Path $path "$appName\Properties\AssemblyInfo.cs")
+
+Write-Host "Reading '$assemblyFile'"
+
+$content = [IO.File]::ReadAllText($assemblyFile)
+
+$regex = new-object System.Text.RegularExpressions.Regex ('(AssemblyVersion(Attribute)?\s*\(\s*\")(.*)(\"\s*\))',
+         [System.Text.RegularExpressions.RegexOptions]::MultiLine)
+
+$version = $null
+$match = $regex.Match($content)
+if($match.Success) {
+    $version = $match.groups[3].value
+}
+
+Write-Host "Updating current AssemblyVersion '$version' to '$buildNumber'"
+
+# update assembly info
+$content = $regex.Replace($content, '${1}' + $buildNumber + '${4}')
+[IO.File]::WriteAllText($assemblyFile, $content)
